5a79e7d947f451382cad382de01c1493d263cb85
Evyatar Rosner <me@evyatarrosner.com>
2021-08-03 04:17:39 +0300

    update react version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-
-    render() {
-        return (
-            <div className="App">
-                Your code here
-            </div>
-        );
-    }
-
+function App() {
+  return (
+    <div className="App">
+      Your code here
+    </div>
+  );
 }
 
 export default App;
